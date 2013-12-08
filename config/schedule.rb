89a1ca3ece88f35e4efde8ce9159adce2807c280
sanemat <o.gata.ken@gmail.com>
2013-12-08 20:46:05 +0900

    Use cd and bundle exec

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,6 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
-set :whenever_command, "bundle exec whenever"
 every '* * * * *' do
-  command %Q!echo '姿勢正しく' | terminal-notifier!
+  command %Q!cd #{Dir.pwd} && echo '姿勢正しく' | bundle exec terminal-notifier!
 end
